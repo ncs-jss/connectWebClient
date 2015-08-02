fe5094885fb26a4eaa475ec553490d183a183b3f
Nikhil Srivastava <niksrc@gmail.com>
2015-08-02 05:33:21 +0530

    Add routes for notice and profiles Add ngFileUpload as dependency

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('connectApp', ['ngRoute','LocalStorageModule'])
+angular.module('connectApp', ['ngRoute','LocalStorageModule','ngFileUpload'])
 
 .config(function($routeProvider,localStorageServiceProvider,$httpProvider){
 
@@ -7,14 +7,65 @@ angular.module('connectApp', ['ngRoute','LocalStorageModule'])
 
     $routeProvider
     .when("/",{
-        templateUrl: "app/views/app.html",
-        controller: "AuthController",
-    });
+        templateUrl: "app/views/notices.html",
+        controller: "NoticeListController",
+    })
+    .when("/search",{
+        templateUrl: "app/views/notices.html",
+        controller: "NoticeSearchController",
+    })
+    .when("/academics",{
+        templateUrl: "app/views/notices.html",
+        controller: "NoticeListController",
+    })
+    .when("/administration",{
+        templateUrl: "app/views/notices.html",
+        controller: "NoticeListController",
+    })
+    .when("/events",{
+        templateUrl: "app/views/notices.html",
+        controller: "NoticeListController",
+    })
+    .when("/placements",{
+        templateUrl: "app/views/notices.html",
+        controller: "NoticeListController",
+    })
+    .when("/misc",{
+        templateUrl: "app/views/notices.html",
+        controller: "NoticeListController",
+    })
+    .when("/starred",{
+        templateUrl: "app/views/notices.html",
+        controller: "NoticeListController",
+    })
+    .when("/profile",{
+        templateUrl: "app/views/profile.html",
+        controller: "ProfileController",
+    })
+    .when("/profile/:id/edit",{
+        templateUrl: "app/views/profileEdit.html",
+        controller: "ProfileEditController",
+    })
+    .when("/notice/:id",{
+        templateUrl: "app/views/notice.html",
+        controller: "NoticeController",
+    })
+    .when("/notice/:id/edit",{
+        templateUrl: "app/views/noticeEdit.html",
+        controller: "NoticeEditController",
+    })
+    .when("/notice/new",{
+        templateUrl: "app/views/noticeNew.html",
+        controller: "NoticeController",
+    })
+    .when("/logout",{
+        controller: "LogoutController",
+    })
 
     $httpProvider.interceptors.push('authInterceptor');
 
 })
 
 .run(function($rootScope){
-    $rootScope.apiBaseUrl = 'http://54.173.150.90:8000' ;
+    $rootScope.apiBaseUrl = 'http://54.173.150.90:8000/api' ;
 })
