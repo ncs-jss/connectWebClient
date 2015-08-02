93b7c5762a4d9700ec5b33e4e74823c85ec20449
Nikhil Srivastava <niksrc@gmail.com>
2015-08-02 19:53:28 +0530

    Fix redirect on token expiration and add auth service

--- public/app/services.js
+++ public/app/services.js
@@ -1,35 +1,138 @@
 angular.module('connectApp')
 
-.factory('authInterceptor',['$q','$injector','$location',function($q,$injector,$location){
+.factory('authInterceptor',['$q','$window',function($q,$window){
     return{
         request: function(config){
-            var storageService = $injector.get('localStorageService');
-            if(storageService.get('auth_token')){
-               var token = storageService.get('auth_token');
+            if(localStorage.getItem('auth_token')){
+               var token = localStorage.getItem('auth_token');
             }
             if(token)
                 config.headers.Authorization = 'JWT ' + token ;
             return config;
         },
-        responseErr: function(response){
+        responseError: function(response){
             if(response.status === 401){
-                storageService.remove('auth_token');
-                $location.path("/");
+                localStorage.clear();
+                $window.location.href = '/';
             }
             $q.reject(response);
         }
     }
 }])
 
-.factory('Auth',['localStorageService','$http','$location','$rootScope',function(localStorageService,$http,$location,$rootScope){
+.factory('Auth',['$http','$window','$rootScope',function($http,$window,$rootScope){
 
     return {
         logout:logout
     };
 
     function logout(){
-        localStorageService.remove('auth_token');
-        localStorageService.remove('type');
-        $location.path("/");
+        localStorage.clear();
+        $window.location.href = '/';
+    }
+}])
+
+.factory('Notice',['$http','$rootScope','Upload',function($http,$rootScope,Upload){
+
+    return {
+        fetch : fetch,
+        fetchAll : fetchAll,
+        remove : removeNotice,
+        save : save,
+        update : update,
+        toggleStar : toggleStarred
+    };
+    function fetchAll(params,callback){
+        $http.get($rootScope.apiBaseUrl+'/notice/',{
+            params: params
+        })
+        .success(function(data,status){
+            callback({error:false},data);
+        })
+        .error(function(data,status){
+            callback({error:true},{msg:'Unable to connect please retry'});
+        })
+    }
+
+    function removeNotice(id,callback){
+        $http.delete($rootScope.apiBaseUrl+'/notice/'+id)
+        .success(function(data,status){
+            callback({error:false},data);
+        })
+        .error(function(data,status){
+            callback({error:true},{msg:'Unable to connect please retry'});
+        })
+    }
+
+    function fetch(id,callback){
+        $http.get($rootScope.apiBaseUrl+'/notice/'+id)
+        .success(function(data,status){
+            callback({error:false},data);
+        })
+        .error(function(data,status){
+            callback({error:true},{msg:'Unable to connect please retry'});
+        })
+    }
+
+    function save(params,file,callback){
+            Upload.upload({
+                url: $rootScope.baseUrl+'/notice/',
+                fields: params,
+                file: file,
+                fileFormDataName:'file_attached'
+            })
+            .success(function(data,status){
+                callback({error:false},data);
+            })
+            .error(function(data,status){
+                callback({error:true},{msg:'Unable to connect please retry'});
+            })
+    }
+
+    function update(params,file,id,callback){
+        if(file !== undefined){
+            Upload.upload({
+                url: $rootScope.apiBaseUrl+'/notice/'+id,
+                fields: params,
+                file: file,
+                fileFormDataName:'file_attached'
+            })
+            .success(function(data,status){
+                callback({error:false},data);
+            })
+            .error(function(data,status){
+                callback({error:true},{msg:'Unable to connect please retry'});
+            })
+        }else{
+            $http.post($rootScope.apiBaseUrl+'/samples/'+id,params)
+            .success(function(data,status){
+                callback({error:false},data);
+            })
+            .error(function(data,status){
+                callback({error:true},{msg:'Unable to connect please retry'});
+            })
+        }
+    }
+
+    function toggleStarred(id,callback){
+        $http.post($rootScope.apiBaseUrl+'/starred/',{notice:id})
+        .success(function(data,status){
+            callback({error:false},data);
+        })
+        .error(function(data,status){
+            callback({error:true},{msg:'Unable to connect please retry'});
+        })
+    }
+
+    function fetchAllStarred(params,callback){
+        $http.get($rootScope.apiBaseUrl+'/starred/',{
+            params: params
+        })
+        .success(function(data,status){
+            callback({error:false},data);
+        })
+        .error(function(data,status){
+            callback({error:true},{msg:'Unable to connect please retry'});
+        })
     }
 }])
