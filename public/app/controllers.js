1af4583c2992bbc087291bc5fc45513310e0b4b3
Nikhil Srivastava <niksrc@gmail.com>
2015-08-05 23:37:41 +0530

    Add search feature and updated app home with correct urls

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -72,3 +72,65 @@ angular.module('connectApp')
 
     search();
 }])
+
+.controller('NoticeSearchController',['$scope','Notice','$routeParams',function($scope,Notice,$routeParams){
+
+    $scope.params =  $routeParams;
+
+    $scope.showNothing = false;
+
+
+    $scope.paginate = function (direction){
+        if(direction > 0){
+            $scope.params.page = $scope.pagination.nextPage;
+            search();
+        }
+        else{
+            $scope.params.page = $scope.pagination.prevPage;
+            search();
+        }
+    }
+
+    function search(){
+
+        Notice.fetchAll($scope.params,function(err,data){
+            $scope.showNothing = false;
+            $scope.count = +data.count;
+            $scope.pagination = {
+                prev:false,
+                next:false
+            };
+
+            if(data.results.length > 0){
+                if(data.previous){
+                    $scope.pagination.prev = true;
+                    if(data.previous.match(/page=\d/))
+                        $scope.pagination.prevPage = +data.previous.match(/page=\d/).join().split('=')[1];
+                    else
+                        $scope.pagination.prevPage = 1;
+
+                }else
+                    $scope.pagination.prevPage = 1;
+
+                if(data.next){
+                    $scope.pagination.next = true;
+                    $scope.pagination.nextPage = +data.next.match(/page=\d/).join().split('=')[1];
+                }
+
+                if($scope.pagination.prevPage === 1 && !$scope.pagination.prev)
+                    $scope.pagination.start = 1;
+                else
+                    $scope.pagination.start = $scope.pagination.prevPage * 15 +1;
+
+                $scope.pagination.end = $scope.pagination.start + data.results.length -1;
+
+                $scope.notices = data.results;
+
+            }else
+                $scope.showNothing = true;
+
+        })
+    }
+
+    search();
+}])
